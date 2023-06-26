d2f1079e90da9875f72e899131127f7bb3af16a4
xiaoou <16024291+xiaoouwang@users.noreply.github.com>
2023-06-26 15:26:02 +0200

    regulat update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
-rm -rf .git
-git init
+# rm -rf .git
+# git init
 git add .
-git commit -m "my personal rep again, bye bye work"
-git remote add origin https://github.com/xiaoouwang/xiaoouwang.github.io.git
-git push -u --force origin master
+git commit -m "regulat update"
+# git remote add origin https://github.com/xiaoouwang/xiaoouwang.github.io.git
+# git push -u --force origin master
+git push origin master
