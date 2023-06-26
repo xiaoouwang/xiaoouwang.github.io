621a6c993b7b07a49527c6e3ce47beb459dba095
xiaoou <16024291+xiaoouwang@users.noreply.github.com>
2023-06-26 15:26:39 +0200

    regular update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # rm -rf .git
 # git init
 git add .
-git commit -m "regulat update"
+git commit -m "regular update"
 # git remote add origin https://github.com/xiaoouwang/xiaoouwang.github.io.git
 # git push -u --force origin master
 git push origin master
